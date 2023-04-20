93437c4bdf05d096be9ab5790e61289a74c13b0a
molfaryura <oleksandrr1996@protonmail.com>
2023-04-20 13:51:30 +0300

    Updated while loop, added game_board

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,20 +1,12 @@
+#!~/pygame/env/bin/python3
+
 """Tetris 2D Game"""
 
 import pygame
 
+import random
+
 from blocks import Block
-
-
-def draw_grid(columns, rows, grid_size, x_gap, y_gap):
-    """Draws rectangles to fill the screen"""
-
-    for y_pos in range(rows):
-        for x_pos in range(columns):
-            pygame.draw.rect(screen,
-                             (100, 100, 100),
-                             [x_pos*grid_size+x_gap, y_pos*grid_size+y_gap, grid_size, grid_size],
-                             1)
-
 
 GRID_SIZE = 30
 
@@ -29,27 +21,38 @@ y_gap_ = (screen.get_height() - rows_ * GRID_SIZE) // 2
 pygame.display.set_caption('Tetris')
 
 GAME_OVER = False
-block = Block(3, 1)
+block = Block((cols - 1) // 2, 0)
 clock = pygame.time.Clock()
-fps = 2
+fps = 10
+game_board = []
+
+for i in range(cols):
+    new_col = [(0,0,0) for _ in range(rows_)]
+    game_board.append(new_col)
+
 while not GAME_OVER:
     clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GAME_OVER = True
-    
+            continue
     if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
         block.side_move(-1, cols)
     elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
         block.side_move(1, cols)
+    elif event.type == pygame.KEYDOWN:
+        block.drop_block(rows_, game_board)
 
     screen.fill((0, 0, 0))
 
-    draw_grid(columns=cols, rows=rows_, grid_size=GRID_SIZE, x_gap=x_gap_, y_gap=y_gap_)
-    block.draw_block(GRID_SIZE, x_gap_, y_gap_, screen)
+    Block.draw_game_board(columns=cols, rows=rows_, screen=screen, game_board=game_board, grid_size=GRID_SIZE, x_gap=x_gap_, y_gap=y_gap_)
 
-    if event.type != pygame.KEYDOWN:
-        block.drop_block(rows_)
+    if block:
+        block.draw_block(GRID_SIZE, x_gap_, y_gap_, screen)
+
+        if event.type != pygame.KEYDOWN:
+            if not block.drop_block(rows_, game_board):
+                block = Block(x=random.randint(0, cols - 2), y=0)
 
     pygame.display.update()
 
